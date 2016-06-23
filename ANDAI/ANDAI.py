7431c6f67acccd0799cfc437447af632d30e3bb5
Austin Jenchi <timtim17@users.noreply.github.com>
2016-06-22 19:18:36 -0700

    Fix the build

--- ANDAI/ANDAI.py
+++ ANDAI/ANDAI.py
@@ -20,7 +20,6 @@ try:
     import DownloadANDAI
 except:
     print "[%s ERR]: The module 'DownloadANDAI' could not be imported. Downloading..." % time.strftime("%H:%M:%S")
-    os.mkdir("%s-%d" % "test", 5)
     urllib.urlretrieve("https://raw.githubusercontent.com/Unknown025/A-Nation-Divided-Against-Itself-Launcher/master/DownloadANDAI.py", "DownloadANDAI.py")
     import DownloadANDAI
 try:
